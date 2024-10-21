82a99b3b83ccffedeaca64e251c902e073e12598
ariiqwicaksana <ariiqwicaksana1@gmail.com>
2024-10-21 11:46:58 +0700

    Unity 6 update

--- Gone Astray/Assets/Gone Astray/Scripts/GrapplingGun.cs
+++ Gone Astray/Assets/Gone Astray/Scripts/GrapplingGun.cs
@@ -132,7 +132,7 @@ public class GrapplingGun : MonoBehaviour
                     break;
                 case LaunchType.Transform_Launch:
                     m_rigidbody.gravityScale = 0;
-                    m_rigidbody.velocity = Vector2.zero;
+                    m_rigidbody.linearVelocity = Vector2.zero;
                     break;
             }
         }
